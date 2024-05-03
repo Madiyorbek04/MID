786f686503d19fcb195aa16370aec9d41eedb258
Madiyorbek04 <axmatovmadiyorbek@gmail.com>
2024-05-02 23:20:47 -0700

    Initial Commit

---
+++ MID/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MID
+//
+//  Created by Axmatjonov Madiyorbek on 30.04.2567 (BE).
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
